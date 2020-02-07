9ec323d2535d1b4bcc5a0f56a18a8ef393777577
kengzas1253 <kengzas1253@gmail.com>
2020-02-07 13:33:46 +0700

    Updates

---
+++ precache-manifest.5c08ffc8b4cf09033554a8c33dd7fe1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bda250b1fb60388378d5890e7f1c0f",
+    "url": "/Lab4_2020/index.html"
+  },
+  {
+    "revision": "2966d7823c4c30c33fa4",
+    "url": "/Lab4_2020/static/css/main.ee6f12ed.chunk.css"
+  },
+  {
+    "revision": "45579e9e5933ed6183ab",
+    "url": "/Lab4_2020/static/js/2.d747b774.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/Lab4_2020/static/js/2.d747b774.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2966d7823c4c30c33fa4",
+    "url": "/Lab4_2020/static/js/main.9c97679e.chunk.js"
+  },
+  {
+    "revision": "a585e749bc3cd283cc21",
+    "url": "/Lab4_2020/static/js/runtime-main.6cf32c84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lab4_2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
